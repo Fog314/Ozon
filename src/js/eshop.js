27c35a1cbcf712c3a116f7f21c5247107041b0b7
Fog314 <navimed7@gmail.com>
2020-02-28 18:12:25 +0300

    Cart markup

--- src/js/eshop.js
+++ src/js/eshop.js
@@ -1,41 +1,94 @@
-$('document').ready(()=>{
-    loadGoods();
-});
+(function eshop() {
+    $('document').ready(() => {
+        loadGoods();
+        loadcarts();
+    });
 
-function loadGoods(){///товары из json
-    $.getJSON('../js/goods.json',(data)=>{
-        let out='';
-        for(let key in data){
-            // if(data[key]["name"])
-            out+='<div class="singleGood">';
-            out+='<img src=' + data[key]["image"]+' width: "200px" height: "200px">';
-            out+='</img>';
-            out+='<div class="info-block">';
-            out+='<div class="price-block">';
-            out+=data[key]["price"];
-            out+='</div>';
-            out+='<div class="name-block">';
-            out+=data[key]["name"];
-            out+='</div>';
-            out+='<div class="stars">';
-            out+='<img src=../../pics/starFull.png>';
-            out+='</img>';
-            out+='<img src=../../pics/starFull.png>';
-            out+='</img>';
-            out+='<img src=../../pics/starFull.png>';
-            out+='</img>';
-            out+='<img src=../../pics/starFull.png>';
-            out+='</img>';
-            out+='<img src=../../pics/star.png>';
-            out+='</img>';
-            out+='123К отзывов'
-            out+='</div>';
-            out+='<div class="buttonContainer">';
-            out+='<button>В корзину</button>'
-            out+='</div>';
-            out+='</div>';
-            out+='</div>';
+    function loadGoods() { ///товары из json
+        $.getJSON('../js/goods.json', (data) => {
+            let out = '';
+            for (let key in data) {
+                out += '<div class="singleGood">';
+                out += '<img src=' + data[key]["image"] + ' width: "200px" height: "200px">';
+                out += '</img>';
+                out += '<div class="info-block">';
+                if (data[key]["discount"] != '0') {
+                    out += '<div class=badge>';
+                    out += '<div class=badgeText>';
+                    out += '-' + data[key]["discount"] + '%';
+                    out += '</div>';
+                    out += '</div>';
+                    out += '<div class="price-block">';
+                    out += '<div class="currentPrice" style="display:inline-block; color: #F91155;">';
+                    out += parseFloat(data[key]["price"]) - parseFloat(data[key]["price"]) / 100 * data[key]["discount"] + '₽';
+                    out += '</div>';
+                    out += '<div class="oldPrice" style="display:inline-block; margin-left: 5px; text-decoration: line-through; text-decoration-color: #F91155">';
+                    out += data[key]["price"] + ' ₽';
+                    out += '</div>';
+                    out += '</div>';
+
+                } else {
+                    out += '<div class="price-block">';
+                    out += '<div class="currentPrice">';
+                    out += data[key]["price"] + ' ₽';
+                    out += '</div>';
+                    out += '</div>';
+                }
+                out += '<div class="name-block">';
+                out += data[key]["name"];
+                out += '</div>';
+                out += '<div class="stars">';
+                out += '<img src=../../pics/starFull.png>';
+                out += '</img>';
+                out += '<img src=../../pics/starFull.png>';
+                out += '</img>';
+                out += '<img src=../../pics/starFull.png>';
+                out += '</img>';
+                out += '<img src=../../pics/starFull.png>';
+                out += '</img>';
+                out += '<img src=../../pics/star.png>';
+                out += '</img>';
+                out += ' 123К отзывов'
+                out += '</div>';
+                out += '<div class="buttonContainer">';
+                out += '<button id=' + data[key]["article"] + '>В корзину</button>'
+                out += '</div>';
+                out += '</div>';
+                out += '</div>';
+            }
+            localStorage.setItem('data', JSON.stringify(data));
+            console.log(localStorage.getItem('data'));
+            $('.main').html(out);
+            script();
+        })
+    }
+
+}())
+
+function loadcarts() {
+    let out = '';
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        if (Number(key) > 0) {
+            document.querySelector(".counter").style.display = 'initial';
+            let data = JSON.parse(localStorage.getItem(key));
+            console.log(data);
+            out += '<div class = carts>';
+            out += '<div class = cartsInfo>';
+            out += '<input type="checkbox" id=' + data.article + '>';
+            out += '<div class = "image-container" style="background: url(' + data.image + '); background-size: contain; background-repeat: no-repeat;">';
+            out += '</div>';
+            out += '<label class="name-container" for ="'+data.article+'">';
+            out += data.name;
+            out += '</label>';
+            out += '</div>';
+            out += '<div class="cartsPrice">';
+            out += data.price + ' ₽';
+            out += '</div>';
+            out += '</div>';
+            out += '<div class="FinalPrice">';
+            out += '</div>';
         }
-        $('.main').html(out);
-    })
+    }
+    $('.cartsContainer').html(out);
 }
---
+++ src/js/scripts.js
@@ -0,0 +1,85 @@
+function script() {
+    let numTimes = localStorage.getItem("counter");
+    if (numTimes == null) {
+        numTimes = 0;
+    } else {
+        numTimes = parseInt(numTimes, 10);
+    }
+
+    document.querySelector(".counter").textContent = localStorage.getItem('counter');
+    console.log(parseFloat(numTimes % 10));
+    switch (numTimes) {
+        case (0):
+            document.querySelector(".objects").textContent = localStorage.getItem('counter') + ' товаров';
+            break;
+        case (1):
+            document.querySelector(".objects").textContent = localStorage.getItem('counter') + ' товар';
+            break;
+        case (2):
+            document.querySelector(".objects").textContent = localStorage.getItem('counter') + ' товара';
+            break;
+        case (3):
+            document.querySelector(".objects").textContent = localStorage.getItem('counter') + ' товара';
+            break;
+        case (4):
+            document.querySelector(".objects").textContent = localStorage.getItem('counter') + ' товара';
+            break;
+        case (5):
+            document.querySelector(".objects").textContent = localStorage.getItem('counter') + ' товаров';
+            break;
+        case (6):
+            document.querySelector(".objects").textContent = localStorage.getItem('counter') + ' товаров';
+            break;
+        case (7):
+            document.querySelector(".objects").textContent = localStorage.getItem('counter') + ' товаров';
+            break;
+        case (8):
+            document.querySelector(".objects").textContent = localStorage.getItem('counter') + ' товаров';
+            break;
+        case (9):
+            document.querySelector(".objects").textContent = localStorage.getItem('counter') + ' товаров';
+            break;
+        default:
+            break;
+    }
+
+    let buttons = document.querySelectorAll(".buttonContainer button");
+    for (let button of buttons) {
+        console.log(button.getAttribute('id'));
+        if (localStorage.getItem(button.getAttribute('id')) != null) {
+            button.textContent = 'В корзине';
+            button.style.color = '#005BFF';
+            button.style.background = '#F2F3F5';
+            console.log(button);
+        }
+        button.addEventListener('click', (event) => {
+            if (event.target.textContent == 'В корзину') {
+                event.target.textContent = 'В корзине';
+                numTimes++;
+                localStorage.setItem("counter", (numTimes).toString(10));
+                document.querySelector(".counter").textContent = localStorage.getItem('counter');
+                event.target.style.color = '#005BFF';
+                event.target.style.background = '#F2F3F5';
+
+                let data = JSON.parse(localStorage.getItem("data"));
+                localStorage.setItem(event.target.getAttribute('id'), JSON.stringify(data[event.target.getAttribute('id')]));
+                if (document.querySelector(".counter").textContent != '0'){
+                    document.querySelector(".counter").style.display = 'initial';
+                }
+            } else if (event.target.textContent == 'В корзине') {
+                event.target.textContent = 'В корзину';
+                numTimes--;
+                localStorage.setItem("counter", (numTimes).toString(10));
+                localStorage.removeItem(event.target.getAttribute('id'));
+                document.querySelector(".counter").textContent = localStorage.getItem('counter');
+                event.target.style.color = '#FFFFFF';
+                event.target.style.background = '#005BFF';
+                if (document.querySelector(".counter").textContent == '0'){
+                    document.querySelector(".counter").style.display = 'none';
+                }
+            }
+
+        })
+    }
+
+}
